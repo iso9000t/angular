f849179424b3e6024cbc43eb60afbfc54e36ae9a
Alexander Grigorev <sasha.gr81@gmail.com>
2023-10-21 02:07:20 -0300

    feat: create the project, configure ESlint-Airbnb rules, TS-rules configured

--- youtube-client-app/src/app/app.component.spec.ts
+++ youtube-client-app/src/app/app.component.spec.ts
@@ -1,29 +1,30 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+import { AppComponent } from "./app.component";
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+describe("AppComponent", () => {
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [RouterTestingModule],
+        declarations: [AppComponent]
+    }));
 
-  it(`should have as title 'youtube-client-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('youtube-client-app');
-  });
+    it("should create the app", () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('youtube-client-app app is running!');
-  });
+    it("should have as title 'youtube-client-app'", () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual("youtube-client-app");
+    });
+
+    it("should render title", () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector(".content span")?.textContent).toContain("youtube-client-app app is running!");
+    });
 });
