53cc5fb2d26330b9a7803f77682f68b38663fe82
Alexander Grigorev <sasha.gr81@gmail.com>
2023-10-21 18:09:26 -0300

    feat: add header, search-results and search-item components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from "./header/header/header.component";
+import { SearchItemComponent } from "./search/search-item/search-item.component";
+import { SearchResultsComponent } from "./search/search-results/search-results.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        SearchResultsComponent,
+        SearchItemComponent
     ],
     imports: [
         BrowserModule,
