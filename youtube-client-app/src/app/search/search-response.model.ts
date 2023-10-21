8aac503add12405391efe115fbe3a8f357b9c45b
Alexander Grigorev <sasha.gr81@gmail.com>
2023-10-21 20:28:07 -0300

    feat: add all the necessary interfaces based on responce.json

---
+++ youtube-client-app/src/app/search/search-response.model.ts
@@ -0,0 +1,14 @@
+import { SearchItem } from "./search-item.model";
+
+export interface SearchResponse {
+  TODO: string;
+  kind: string;
+  etag: string;
+  pageInfo: PageInfo;
+  items: SearchItem[];
+}
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
