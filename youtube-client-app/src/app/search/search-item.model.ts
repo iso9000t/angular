27295178207f1e0b499c5925b37d7130e7d8c8cf
Alexander Grigorev <sasha.gr81@gmail.com>
2023-10-21 21:45:11 -0300

    refactor: refactor code according to ESlint  specifications

--- youtube-client-app/src/app/search/search-item.model.ts
+++ youtube-client-app/src/app/search/search-item.model.ts
@@ -1,51 +1,51 @@
 export interface SearchItem {
-  kind: string;
-  etag: string;
-  id: string;
-  snippet: Snippet;
-  statistics: Statistics;
+    kind: string;
+    etag: string;
+    id: string;
+    snippet: Snippet;
+    statistics: Statistics;
 }
 
 export interface Snippet {
-  publishedAt: string;
-  channelId: string;
-  title: string;
-  description: string;
-  thumbnails: Thumbnails;
-  channelTitle: string;
-  tags: string[];
-  categoryId: string;
-  liveBroadcastContent: string;
-  localized: Localized;
-  defaultAudioLanguage: string;
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: Thumbnails;
+    channelTitle: string;
+    tags: string[];
+    categoryId: string;
+    liveBroadcastContent: string;
+    localized: Localized;
+    defaultAudioLanguage: string;
 }
 
 // Interface for the 'thumbnails' property inside 'snippet'
 export interface Thumbnails {
-  default: Thumbnail;
-  medium: Thumbnail;
-  high: Thumbnail;
-  standard: Thumbnail;
-  maxres: Thumbnail;
+    default: Thumbnail;
+    medium: Thumbnail;
+    high: Thumbnail;
+    standard: Thumbnail;
+    maxres: Thumbnail;
 }
 
 // Interface for each individual thumbnail type
 export interface Thumbnail {
-  url: string;
-  width: number;
-  height: number;
+    url: string;
+    width: number;
+    height: number;
 }
 
 // Interface for the 'localized' property inside 'snippet'
 export interface Localized {
-  title: string;
-  description: string;
+    title: string;
+    description: string;
 }
 
 export interface Statistics {
-  viewCount: string;
-  likeCount: string;
-  dislikeCount: string;
-  favoriteCount: string;
-  commentCount: string;
+    viewCount: string;
+    likeCount: string;
+    dislikeCount: string;
+    favoriteCount: string;
+    commentCount: string;
 }
--- youtube-client-app/src/main.ts
+++ youtube-client-app/src/main.ts
@@ -2,5 +2,9 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => {
+    // eslint-disable-next-line no-console
+        console.error(err);
+    });
