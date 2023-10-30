4e03c9db7baa0b5d619aec9cfa4ab868e494b5a4
Alexander Grigorev <sasha.gr81@gmail.com>
2023-10-30 16:27:13 -0300

    feat: update the logic so that it works faster, it doesn't re-fetch the data on filter reset

--- youtube-client-app/src/app/search/search-results/search-results.component.ts
+++ youtube-client-app/src/app/search/search-results/search-results.component.ts
@@ -7,62 +7,62 @@ import { SortOrder } from "src/enums/sort.enum";
 import { SearchResponse } from "../search-response.model";
 
 @Component({
-  selector: 'app-search-results',
-  templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss'],
+    selector: "app-search-results",
+    templateUrl: "./search-results.component.html",
+    styleUrls: ["./search-results.component.scss"],
 })
 export class SearchResultsComponent implements OnInit, OnChanges {
-  data: Partial<SearchResponse> = { items: [] };
+    displayData: Partial<SearchResponse> = { items: [] };
+    originalData: Partial<SearchResponse> = { items: [] };
 
-  @Input() filterKeyword = '';
+    @Input() filterKeyword = "";
 
-  @Input() sortOrder: SortOrder = SortOrder.NONE;
+    @Input() sortOrder: SortOrder = SortOrder.NONE;
 
-  constructor(private dataService: DataService) {}
+    constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
-    this.dataService.fetchData().subscribe((response) => {
-      this.data = response;
-      this.sortData();
-    });
-  }
+    ngOnInit(): void {
+        this.dataService.fetchData().subscribe((response) => {
+            this.originalData = response;
+            this.displayData = structuredClone(this.originalData);
+            this.sortData();
+        });
+    }
 
-  ngOnChanges(): void {
-    this.dataService.fetchData().subscribe((response) => {
-      this.data = response;
-      this.sortData();
-    });
-    this.sortData();
-  }
+    ngOnChanges(): void {
+        this.displayData = structuredClone(this.originalData);
+
+        this.sortData();
+    }
 
     sortData() {
-     if (!this.data.items) {
-       return;
-     }
-    const sortedData = [...this.data.items];
-    if (this.sortOrder === SortOrder.NONE) {
-      return;
+        if (!this.displayData.items) {
+            return;
+        }
+        const sortedData = [...this.displayData.items];
+        if (this.sortOrder === SortOrder.NONE) {
+            return;
+        }
+        sortedData.sort((a, b) => {
+            const dateA = new Date(a.snippet.publishedAt).getTime();
+            const dateB = new Date(b.snippet.publishedAt).getTime();
+            const viewsA = +a.statistics.viewCount;
+            const viewsB = +b.statistics.viewCount;
+
+            switch (this.sortOrder) {
+                case SortOrder.DATE_ASC:
+                    return dateA - dateB;
+                case SortOrder.DATE_DESC:
+                    return dateB - dateA;
+                case SortOrder.VIEWS_ASC:
+                    return viewsA - viewsB;
+                case SortOrder.VIEWS_DESC:
+                    return viewsB - viewsA;
+                default:
+                    return 0;
+            }
+        });
+
+        this.displayData.items = sortedData;
     }
-    sortedData.sort((a, b) => {
-      const dateA = new Date(a.snippet.publishedAt).getTime();
-      const dateB = new Date(b.snippet.publishedAt).getTime();
-      const viewsA = +a.statistics.viewCount;
-      const viewsB = +b.statistics.viewCount;
-
-      switch (this.sortOrder) {
-        case SortOrder.DATE_ASC:
-          return dateA - dateB;
-        case SortOrder.DATE_DESC:
-          return dateB - dateA;
-        case SortOrder.VIEWS_ASC:
-          return viewsA - viewsB;
-        case SortOrder.VIEWS_DESC:
-          return viewsB - viewsA;
-        default:
-          return 0;
-      }
-    });
-
-    this.data.items = sortedData;
-  }
 }
