3a6be5ff365e5ab8c15de3ac8ab77a3dae825a11
Alexander Grigorev <sasha.gr81@gmail.com>
2023-12-09 02:26:04 -0300

    update: change name validator to allow all characters

--- connections/src/app/registration/validators/name.validator.ts
+++ connections/src/app/registration/validators/name.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function nameValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const valid = /^[A-Za-z\s]+$/.test(control.value);
+    const valid = /^[\p{L}\s]+$/u.test(control.value);
     return valid ? null : { invalidName: true };
   };
 }
