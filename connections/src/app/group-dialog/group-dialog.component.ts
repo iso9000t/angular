9038bb1ddb9be926af6997c248e05c1a0431469c
Alexander Grigorev <sasha.gr81@gmail.com>
2023-12-16 19:31:41 -0300

    feat: imrove last update logic and add unsubscribtion ito group dialog page

--- connections/src/app/group-dialog/group-dialog.component.ts
+++ connections/src/app/group-dialog/group-dialog.component.ts
@@ -1,66 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import * as GroupSelectors from '../redux/selectors/groups.selector';
 import * as GroupMessageSelectors from '../redux/selectors/group-message.selector';
 import * as GroupMessageActions from '../redux/actions/group-message.action';
-import * as Useractions from '../redux/actions/user.action';
-import { Observable } from 'rxjs';
-import { UserItem } from '../main/models/user.model';
-import { GroupMessageItem } from '../main/models/group.model';
+import * as UserActions from '../redux/actions/user.action';
+import * as UserSelector from '../redux/selectors/user.selector';
+import { Observable, Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
+import {
+  GroupMessageItem,
+  GroupMessageError,
+} from 'src/app/main/models/group.model'; // Ensure GroupMessageError is imported
 
 @Component({
   selector: 'app-group-dialog',
   templateUrl: './group-dialog.component.html',
   styleUrls: ['./group-dialog.component.scss'],
 })
-export class GroupDialogComponent implements OnInit {
+export class GroupDialogComponent implements OnInit, OnDestroy {
   userUID = localStorage.getItem('uid');
   groupID!: string;
   isGroupCreator: boolean = false;
-  messages$ = this.store.select(GroupMessageSelectors.selectGroupMessages);
-  loading$ = this.store.select(
+
+  messages$: Observable<GroupMessageItem[]> = this.store.select(
+    GroupMessageSelectors.selectGroupMessages
+  );
+  loading$: Observable<boolean> = this.store.select(
     GroupMessageSelectors.selectGroupMessagesLoading
   );
-  error$ = this.store.select(GroupMessageSelectors.selectGroupMessagesError);
-  sortedMessages$!: Observable<GroupMessageItem[]>;
-  latestMessageTimestamp: number = 0 ;
- 
+  error$: Observable<GroupMessageError | null> = this.store.select(
+    GroupMessageSelectors.selectGroupMessagesError
+  );
+  sortedMessages$: Observable<GroupMessageItem[]> = this.store.select(
+    GroupMessageSelectors.selectSortedGroupMessages
+  );
+  latestMessageTimestamp: number = 0;
+  lastUpdated$: Observable<number | null> = this.store.select(
+    GroupMessageSelectors.selectLastFetchedTimestamp
+  );
+  private subscriptions = new Subscription();
 
   constructor(private store: Store, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.groupID = this.route.snapshot.paramMap.get('groupID') || '';
-    this.store.dispatch(
-      GroupMessageActions.loadGroupMessages({ groupId: this.groupID })
+    this.subscribeToLoadGroups();
+    this.subscribeToLoadUsers();
+    this.setupGroupCreatorSubscription();
+    this.setupLatestMessageTimestampSubscription();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private subscribeToLoadGroups() {
+    this.subscriptions.add(
+      this.store
+        .pipe(select(GroupMessageSelectors.selectSortedGroupMessages), take(1))
+        .subscribe((messages) => {
+          if (messages.length === 0) {
+            this.store.dispatch(
+              GroupMessageActions.loadGroupMessages({ groupId: this.groupID })
+            );
+          }
+        })
     );
-    this.store.dispatch(Useractions.loadUsers());
+  }
 
-    this.store
-      .select(GroupSelectors.isUserGroupCreator, { groupId: this.groupID })
-      .subscribe((isCreator) => {
-        this.isGroupCreator = isCreator;
-        console.log('Is user the group creator?', isCreator);
-      });
-
-    this.sortedMessages$ = this.store.select(
-      GroupMessageSelectors.selectSortedGroupMessages
+  private subscribeToLoadUsers() {
+    this.subscriptions.add(
+      this.store
+        .pipe(select(UserSelector.selectUsers), take(1))
+        .subscribe((users) => {
+          if (users.length === 0) {
+            this.store.dispatch(UserActions.loadUsers());
+          }
+        })
     );
+  }
 
-    // Subscribe to the latest message timestamp selector
-    this.store
-      .select(GroupMessageSelectors.selectLatestMessageTimestamp)
-      .subscribe((timestampString) => {
-        // Convert the timestamp to a number here
-        this.latestMessageTimestamp = timestampString
-          ? Number(timestampString)
-          : 0;
-        console.log('Latest message timestamp:', this.latestMessageTimestamp);
-      });
+  private setupGroupCreatorSubscription() {
+    this.subscriptions.add(
+      this.store
+        .select(GroupSelectors.isUserGroupCreator, { groupId: this.groupID })
+        .subscribe((isCreator) => {
+          this.isGroupCreator = isCreator;
+        })
+    );
+  }
+
+  private setupLatestMessageTimestampSubscription() {
+    this.subscriptions.add(
+      this.store
+        .select(GroupMessageSelectors.selectLatestMessageTimestamp)
+        .subscribe((timestamp) => {
+          this.latestMessageTimestamp = timestamp ? Number(timestamp) : 0;
+        })
+    );
   }
 
   onUpdate() {
-    // Use the latestMessageTimestamp directly in your dispatch
     this.store.dispatch(
       GroupMessageActions.loadGroupMessagesSince({
         groupId: this.groupID,
