37b7cf013d77395ec7bc4efb16ae606014cecd49
Alexander Grigorev <sasha.gr81@gmail.com>
2023-12-19 05:14:16 -0300

    feat: add a night theme

--- connections/src/app/app.component.ts
+++ connections/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'connections';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+    const theme = localStorage.getItem('theme') || 'light';
+    document.body.classList.toggle('dark-theme', theme === 'dark');
+  }
 }
--- connections/src/app/header/header/header.component.ts
+++ connections/src/app/header/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  themeIcon = 'light_mode'; // default icon
 
+  ngOnInit() {
+    this.applyTheme();
+  }
+
+  toggleTheme() {
+    const currentTheme = localStorage.getItem('theme') || 'light';
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    localStorage.setItem('theme', newTheme);
+    this.applyTheme();
+  }
+
+  applyTheme() {
+    const theme = localStorage.getItem('theme') || 'light';
+    document.body.classList.toggle('dark-theme', theme === 'dark');
+    this.themeIcon = theme === 'light' ? 'light_mode' : 'dark_mode';
+  }
 }
